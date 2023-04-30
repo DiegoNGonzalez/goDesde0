db8596a1a194f016ee7bc375b9e4f78676d6ad38
DiegoNGonzalez <diegonzalezv93@gmail.com>
2023-04-30 20:10:42 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/DiegoNGonzalez/goDesde0/variables"
+	"runtime"
 )
 
 func main(){
 
-	estado,texto := variables.ConviertoATexto(15700)
+	/*estado,texto := variables.ConviertoATexto(15700)
 	fmt.Println(estado)
 	fmt.Println(texto)
+*/
+	if os:= runtime.GOOS; os=="Linux." || os== "Os X."{
+		fmt.Println("Esto no es Windows, es ", os)
 
+	}else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os:= runtime.GOOS; os{
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	} 
 }
