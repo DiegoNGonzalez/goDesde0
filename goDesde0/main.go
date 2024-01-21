357cfb0d348ff076516d5611c935da7cc71972ab
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-21 00:08:25 -0300

    condicionales

--- goDesde0/main.go
+++ goDesde0/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"fmt" 
-	"github.com/diegoNGonzalez/goDesde0/variables"
+	"fmt"
+	"runtime"
 )
-func main(){
-	fmt.Println("Hola Mundo")
+
+func main() {
+	/*fmt.Println("Hola Mundo")
 	variables.ShowInt()
 	variables.RestOfVariables()
 	state,text := variables.ConverToText(35)
 	fmt.Println("State: ", state)
-	fmt.Println("Text convert: ", text)
+	fmt.Println("Text convert: ", text)*/
+
+	if os := runtime.GOOS; os == "Linux." || os == "Os X. " {
+		fmt.Println("This doesn't is Windows")
+
+	} else {
+		fmt.Println("This is Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux":
+		fmt.Println("This is Linux")
+	case "Os X":
+		fmt.Println("This is Os X")
+	default:
+		fmt.Printf("%s. \n", os)
+	}
 }
